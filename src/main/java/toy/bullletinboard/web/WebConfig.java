e6d19a6d96ab1360d723cb907cc786f5a848fe93
WanDuKong-pea <sophisticateo_o@naver.com>
2023-07-19 04:37:06 +0900

    SpringToyProject-에러 페이지 작성, 요청 응답 로그 남기기

--- src/main/java/toy/bullletinboard/web/WebConfig.java
+++ src/main/java/toy/bullletinboard/web/WebConfig.java
@@ -3,6 +3,7 @@ package toy.bullletinboard.web;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import toy.bullletinboard.web.interceptor.LogInterceptor;
 import toy.bullletinboard.web.interceptor.LoginCheckInterceptor;
 
 @Configuration
@@ -11,12 +12,17 @@ public class WebConfig implements WebMvcConfigurer { //implements WebMvcConfigur
 
     @Override //WebMvcConfigurer 의 인터셉터 등록 메서드
     public void addInterceptors(InterceptorRegistry registry){
+        //Log 인터셉터 등록
+        registry.addInterceptor(new LogInterceptor())
+                .order(1) //인터셉터 호출 순서. 낮을 수록 먼저 호출
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**","/*.ico","/boards/error"); //인터셉터에서 제외할 패턴 지정
 
         //로그인 인증 체크 인테셉터 등록
         registry.addInterceptor(new LoginCheckInterceptor())
                 .order(2)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/css/**","/*.ico","/error",
-                        "/boards","/member/**");
+                        "/boards","/member/**","/boards/error");
     }
 }
